4cf485e290041fb479dd2a9cb4942d7f4010111e
Nancy Sakhiya <sakhiyanancy@gmail.com>
2025-02-12 12:10:03 +0530

    database tabels created

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -29,3 +29,50 @@ CREATE TABLE user_sessions (
 ); 
 
 
+
+
+CREATE TABLE student_info(
+    Student_ID VARCHAR(250) PRIMARY KEY,
+    Email VARCHAR(300) NOT NULL,
+    DOB DATE NOT NULL,
+    Phone_No CHAR(10) NOT NULL,
+    Addresss text NOT NULL,
+    Department VARCHAR(50),
+    Current_semester VARCHAR(30),
+    Enrollment_Year INT,
+    FOREIGN KEY (Department) REFERENCES departments(department_id),
+    FOREIGN KEY (Current_semester) REFERENCES semesters(semester_id)
+);
+
+CREATE TABLE faculty_info(
+    Faculty_ID VARCHAR(250) PRIMARY KEY,
+    Email VARCHAR(300) NOT NULL,
+    Department VARCHAR(50) NOT NULL,
+    Phone_No CHAR(10) NOT NULL,
+    Office text NOT NULL,
+    Specialization VARCHAR(300),
+    Year_of_exp INT NOT NULL,
+    Office_hour TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (Department) REFERENCES departments(department_id),
+    FOREIGN KEY (Course_ID) REFERENCES courses(course_id)
+);
+
+CREATE TABLE departments(
+    department_id VARCHAR(50) PRIMARY KEY,
+    department_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE semesters (
+    semester_id VARCHAR(30) PRIMARY KEY,
+    semester_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE courses (
+    course_id VARCHAR(50) PRIMARY KEY,
+    course_name VARCHAR(200) NOT NULL,
+    department_id VARCHAR(50),
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
+
+
+
